1fef579973d80be2efe94a2d78bb4ff6c7d67370
Frogleim <92037197+Frogleim@users.noreply.github.com>
2024-08-02 22:15:38 +0400

    puzzle GPU

--- sol_finder.py
+++ sol_finder.py
@@ -1,17 +1,26 @@
 import time
-from web3 import Web3
-from eth_account import Account
+import requests
+from bitcoin import *
 
 def generate_private_key(start_key: int) -> str:
     key = start_key.to_bytes(32, byteorder='big')
     return key.hex()
 
-def check_balance(client: Web3, address: str) -> float:
+def get_litecoin_address(private_key_hex: str) -> str:
+    private_key = private_key_hex
+    public_key = privtopub(private_key)
+    address = pubtoaddr(public_key, magicbyte=48)  # 48 is the version byte for Litecoin mainnet addresses
+    return address
+
+def check_balance(address: str) -> float:
+    url = f'https://chain.so/api/v2/get_address_balance/LTC/{address}'
     while True:
         try:
-            balance_wei = client.eth.get_balance(address)
-            balance_bnb = Web3.from_wei(balance_wei, 'ether')
-            return balance_bnb
+            response = requests.get(url)
+            data = response.json()
+            print(data)
+            balance_ltc = float(data['data']['confirmed_balance'])
+            return balance_ltc
         except Exception as e:
             print(f"Error: {e}. Retrying...")
             time.sleep(1)  # Delay to handle rate limit
@@ -21,18 +30,16 @@ def save_to_file(data: str, filename: str = "output.txt"):
         file.write(data + '\n')
 
 def main():
-    client = Web3(Web3.HTTPProvider("https://bsc-dataseed.binance.org/"))
-    start_key = 800000000
-    stop_key = 0xfffffffff
-    filename = "BNB.txt"
+    start_key = 400
+    stop_key = 0x7ff
+    filename = "LTC.txt"
 
     for key_int in range(start_key, stop_key + 1):
         private_key_hex = generate_private_key(key_int)
-        account = Account.from_key(private_key_hex)
-        address = account.address
+        address = get_litecoin_address(private_key_hex)
         
-        balance = check_balance(client, address)
-        data = f'Address: {address} | Balance: {balance} BNB | Private Key: {private_key_hex}'
+        balance = check_balance(address)
+        data = f'Address: {address} | Balance: {balance} LTC | Private Key: {private_key_hex}'
         print(data)
         save_to_file(data, filename)
         time.sleep(1)  # Delay to handle rate limit
